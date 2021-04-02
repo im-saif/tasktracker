29f208ab4b4de6f05f1b94fd4475dc3850350603
Saif Eldin <saifeldin402@gmail.com>
2021-04-02 12:49:07 +0300

    fixed api

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import About from './components/About'
 function App() {
   const [showAddTask,setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
+  
 
   useEffect( () => {
     const getTasks = async() => {
@@ -19,19 +20,19 @@ function App() {
   }, [])
 
   const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks')
+    const res = await fetch('https://tasktracker-api.herokuapp.com/tasks')
     const data = await res.json()
     return data
   }
 
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const res = await fetch(`https://tasktracker-api.herokuapp.com/tasks/${id}`)
     const data = await res.json()
     return data
   }
 
   const addTask = async (task) =>{
-    const res = await fetch('http://localhost:5000/tasks', {
+    const res = await fetch('https://tasktracker-api.herokuapp.com/tasks', {
       method: 'POST', 
       headers: {
         'Content-type': 'application/json'
@@ -43,14 +44,14 @@ function App() {
   }
 
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE',})
+    await fetch(`https://tasktracker-api.herokuapp.com/tasks/${id}`, {method: 'DELETE',})
     setTasks(tasks.filter( (task) => task.id !== id))
   }
 
   const toggleReminder = async (id) => {
     const taskToToggle =  await fetchTask(id)
     const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    const res = await fetch(`https://tasktracker-api.herokuapp.com/tasks/${id}`, {
       method: 'PUT',
       headers: {
         'Content-type':'application/json'
